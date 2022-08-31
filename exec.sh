04cec59e789249100d6831b46678fb30f0ff300e
Zhang <zhang@i22-yzhang.local>
2022-08-31 19:16:06 +0900

    more universal

--- exec.sh
+++ exec.sh
@@ -5,7 +5,8 @@ cd ./repo
 pip install -r requirements.txt
 pip install .
 cd docs/tutorials
-python train_painn_r2.py
+python $@
+return $?
 
 # python -u $APPDIR/main_qm9.py --num_workers 4 $@
 # return $?
